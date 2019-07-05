3cb32e36d32710787567dffe5f5bd366e12e99fc
Livia Santos <liviamelq@gmail.com>
2019-07-06 07:14:39 +1000

    Add some event to the service worker

---
+++ course-project/public/sw.js
@@ -0,0 +1,15 @@
+// You have access to a special set of Service Workers events'
+self.addEventListener('install', function(event){
+  console.log('[Service Worker] Installing Service Worker ...', event);
+});
+
+self.addEventListener('activate', function(event){
+  console.log('[Service Worker] Activating Service Worker ...', event);
+  // ensures that the servicer workers are loaded correctly
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  console.log('[Service Worker] Fetching something ...', event)
+  event.respondWith(fetch(event.request));
+})
