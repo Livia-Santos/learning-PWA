8841c933861010ad59fc43d52b0ca442ff365a00
Livia Santos <liviamelq@gmail.com>
2019-07-07 12:09:32 +1000

    Add fetch and promises polyfills for older browsers

--- course-project/public/src/js/app.js
+++ course-project/public/src/js/app.js
@@ -1,11 +1,19 @@
 var deferredPrompt;
 
+// Check if the browser support Promises and use polyfill
+if(!window.Promise) {
+  window.Promise = Promise;
+}
+
 // Service worker registration
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(function() {
       console.log('Service worker registered');
+    })
+    .catch(function(err){
+      console.log(err);
     });
 }
 
